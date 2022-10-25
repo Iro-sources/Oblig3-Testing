f63e07327af56682b7ade6c04d347524bc1bb8b3
Mohamed-codes <mohamediro98@gmail.com>
2022-10-25 21:02:26 +0200

    last commit

---
+++ test_years.py
@@ -0,0 +1,14 @@
+from years import checkLeapYear
+
+def test_is_divisible_by_4():
+    assert checkLeapYear(4) == True
+
+def test_is_divisible_by_100():
+    assert checkLeapYear(100) == False
+
+def test_is_divisible_by_400():
+    assert checkLeapYear(400) == True
+
+def test_is_divisible_by_200():
+    assert checkLeapYear(200) == False
+
